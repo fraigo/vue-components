dbb4c69a3eb1d861a02e47e659d5437a8eda8d2c
Francisco Igor <franciscoigor@gmail.com>
2018-03-27 23:26:59 -0700

    fixed button icon, updated icon

--- js/components/basic/ui-button.js
+++ js/components/basic/ui-button.js
@@ -6,17 +6,21 @@ Vue.component('ui-button', {
       },
       iconSize:{
         type:String,
-        default:32,
+        default:"1.6rem",
       },
       fontSize:{
         type:String,
         default:"1.2rem",
+      },
+      padding:{
+        type:String,
+        default:"8px",
       }
     },
     created:function(){
         
     },
     template:`
-  <button :id="id" :style="style" @click="$emit('click')"><ui-icon v-if="icon" :icon="icon"></ui-icon>&nbsp;<slot></slot></button>
+  <button :id="id" :style="style" @click="$emit('click')"><ui-icon v-if="icon" :icon="icon" :style="{verticalAlign:'middle',fontSize:iconSize}"></ui-icon>&nbsp;<slot></slot></button>
       `
     })
